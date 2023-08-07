fae3732163986d9f1d3084e24b9e70acb18f635c
of-legend <deibi1977@hotmail.com>
2023-08-06 23:35:49 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    HeroDetailsComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule
